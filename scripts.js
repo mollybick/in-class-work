d677f255079224cd3d62696653f7398b074698b9
Molly Bick <mollybick94@gmail.com>
2019-09-19 21:29:13 -0500

    Add recursive greeter function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function greeter(question = "What is your name?") {
   return prompt(question);
+
+  /* If answer is blank, ask again */
+
+  if (answer === "") {
+    greeter(); // recursion
+
+  return answer;
 }
 
+// TODO: resolve issue where h1 is not updated if user gives blank answer
 const answer = greeter();
-document.querySelector("h1").textContent = answer
-
+document.querySelector("h1").textContent = answer;
