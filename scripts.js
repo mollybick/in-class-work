91877f029e3dc2fd5b6f30efa81652d13bae2ed9
Molly Bick <mollybick94@gmail.com>
2019-09-23 21:27:26 -0500

    Fixed greeter bug

--- scripts.js
+++ scripts.js
@@ -4,11 +4,9 @@ function greeter(question = "What is your name?") {
   /* If answer is blank, ask again */
 
   if (answer === "") {
-    greeter(); // recursion
+  return greeter(); // recursion
 
   return answer;
 }
-
-// TODO: resolve issue where h1 is not updated if user gives blank answer
 const answer = greeter();
 document.querySelector("h1").textContent = answer;
