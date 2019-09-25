8caf073284b7b344d5c4e74357e7c61e92824585
Molly Bick <mollybick94@gmail.com>
2019-09-24 20:33:42 -0500

    Use innerhtml to add greater output to root div

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 function greeter(question = "What is your name?") {
-  return prompt(question);
+let answer = prompt(question);
 
   /* If answer is blank, ask again */
 
   if (answer === "") {
-    return greeter(); // recursion
+    return greeter();
+  }
 
   return answer;
 }
-const answer = greeter();
-document.querySelector("h1").textContent = answer;
+// const answer = greeter();
+
+document.querySelector("#root").innerHTML = `<p>${greeter()}</p>`
+
+use innerhtml
