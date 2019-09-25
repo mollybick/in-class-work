3063286cc9b90d7c9af25000b4b7aaf67de6ea36
Molly Bick <mollybick94@gmail.com>
2019-09-24 21:13:00 -0500

    Add JS to toggle the nav menu

--- scripts.js
+++ scripts.js
@@ -1,22 +1,7 @@
-function greeter(question = "What is your name?") {
-let answer = prompt(question);
-
-  /* If answer is blank, ask again */
-
-  if (answer === "") {
-    return greeter();
-  }
-
-  return answer;
-}
-// const answer = greeter();
-
-document.querySelector("#root").innerHTML = `<p>${greeter()}</p>`
-
 const hamburger = document.querySelector(".fa-hamburger");
 const hiddenUL = document.querySelector("ul");
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener("click", function() {
+  hiddenUL.classList.toggle("is-hidden--tablet");
 
-
-})
+});
