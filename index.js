de67cc83d52be9037597e40971c444954b9361bd
Molly Bick <mollybick94@gmail.com>
2019-09-30 19:02:11 -0500

    Updated footer to be dynamic

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-console.log(Header, Nav, Main, Footer);
-
 document.querySelector("#root").innerHTML = `
 ${Header()}
 ${Nav()}
