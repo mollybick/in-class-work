7213f08e83177fcfe3759843dc85377a13b7d8df
Molly Bick <mollybick94@gmail.com>
2019-10-03 20:38:56 -0500

    Refactor state out to store

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-// Below is an example of object destructuring
-
 import { Header, Nav, Main, Footer } from "./components";
-
-const state = {
-  home: {
-    heading: "Home Page",
-    links: ["Home", "About", "Contact", "Blog", "Gallery"]
-  },
-
-  about: {
-    heading: "About Page",
-    links: ["Home", "About", "Contact", "Blog", "Gallery"]
-  },
-
-  contact: {
-    heading: "Contact Page",
-    links: ["Home", "About", "Contact", "Blog", "Gallery"]
-  },
-
-  blog: {
-    heading: "Blog page",
-    links: ["Home", "About", "Contact", "Blog", "Gallery"]
-  },
-
-  gallery: {
-    heading: "Gallery page",
-    links: ["Home", "About", "Contact", "Blog", "Gallery"]
-  }
-};
+import { Home, About, Contact, Blog, Gallery, Links } from "./store";
 
 //The parameter st represents a piece of state.
 
-function render(st = state.home) {
+function render(st = Home) {
   document.querySelector("#root").innerHTML = `
 ${Header(st)}
 ${Nav(st)}
-${Main(st)}
-${Footer(st)}
+${Main()}
+${Footer()}
 `;
 }
 
@@ -48,6 +20,6 @@ for (let i = 0; i < links.length; i += 1) {
   links[i].addEventListener("click", function(event) {
     event.preventDefault();
 
-    render(state[event.target.textContent.toLowerCase()]);
+    render(event.target.textContent);
   });
 }
