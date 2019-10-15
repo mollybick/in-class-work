633699dd9c5700f9c066cb9778fcb7ec6e243f3c
Molly Bick <mollybick94@gmail.com>
2019-10-14 20:44:51 -0500

    Allow user to type in mixed cases for routes

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ ${Footer()}
 
 router
   // Developer's Note: ':page' can be whatever you want to name the key that comes into `params` Object Literal
-  .on(":page", params => render(state[params.page]))
+  .on(":page", params =>
+    render(state[`${params.page.slice(0, 1).toUpperCase()}${params.page.slice(1).toLowerCase()}`])
+  )
   .on("/", render())
   .resolve();
