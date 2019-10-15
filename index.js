9c4314dc3925a1a571c726f8ff30035d1e1e2961
Molly Bick <mollybick94@gmail.com>
2019-10-14 20:35:23 -0500

    Add navigo routing for all routes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import * as state from "./store";
 import Navigo from "navigo";
 const router = new Navigo(location.origin);
 
-
 //The parameter st represents a piece of state.
 
 function render(st = state.Home) {
@@ -24,4 +23,8 @@ ${Footer()}
   );
 }
 
-render();
+router
+  // Developer's Note: ':page' can be whatever you want to name the key that comes into `params` Object Literal
+  .on(":page", params => render(state[params.page]))
+  .on("/", render())
+  .resolve();
